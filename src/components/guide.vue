3e8ada469de380f2decb33067eb25ed83812e5d6
TommcyOWO <tangminghong.tom@gmail.com>
2023-09-11 22:05:02 +0800

    update

--- src/components/guide.vue
+++ src/components/guide.vue
@@ -23,23 +23,16 @@ const push = (async (herf:string) => {
   <nav class="fixed flex h:10vh w:100% align-items:center z:2 bd:blur(5px)">
     <div class="rel w:100% f:18 color:gray-80 left:20">
       
-    <span v-if="webName === 'index'" @click="push('/')" class="m:10px cursor:pointer color:crimson-54">
-      首頁</span>
-    <span v-else @click="push('/')" class="m:10px cursor:pointer color:crimson-54:hover ~300ms|ease-in">
-      首頁</span>
+    <span v-if="webName === 'index'" @click="push('/')" class="m:10px cursor:pointer color:crimson-54">首頁</span>
+    <span v-else @click="push('/')" class="m:10px cursor:pointer color:crimson-54:hover ~300ms|ease-in">首頁</span>
 
-    <span v-if="webName === 'about'" @click="push('/about')" class="m:10px cursor:pointer color:crimson-54">
-      介紹</span>
-    <samp v-else @click="push('/about')" class="m:10px cursor:pointer color:crimson-54:hover ~300ms|ease-in">
-      介紹</samp>
-
-    <span v-if="webName === 'development_introduction'" @click="push('/development_introduction')" class="m:10px cursor:pointer color:crimson-54">
-      開發介紹</span>
-    <samp v-else @click="push('/development_introduction')" class="m:10px cursor:pointer color:crimson-54:hover ~300ms|ease-in">
-      開發介紹</samp>
+    <span v-if="webName === 'about'" @click="push('/about')" class="m:10px cursor:pointer color:crimson-54">介紹</span>
+    <samp v-else @click="push('/about')" class="m:10px cursor:pointer color:crimson-54:hover ~300ms|ease-in">介紹</samp>
+    <span v-if="webName === 'development_introduction'" @click="push('/development_introduction')" class="m:10px cursor:pointer color:crimson-54">開發介紹</span>
+    <samp v-else @click="push('/development_introduction')" class="m:10px cursor:pointer color:crimson-54:hover ~300ms|ease-in">開發介紹</samp>
     </div>
     <div class="rel color:white right:20">
-      <samp class="m:10px cursor:pointer bg:crimson-58:hover ~300ms|ease-in btn" v-if="'ee'==='ee'">singout</samp>
+      <samp class="m:10px cursor:pointer bg:crimson-58:hover ~300ms|ease-in btn" v-if="'ee'==='ee'">Singout</samp>
       <samp class="m:10px cursor:pointer bg:crimson-58:hover ~300ms|ease-in btn" v-else>Logon</samp>  
     </div>
   </nav>
--- src/views/about_me.vue
+++ src/views/about_me.vue
@@ -24,29 +24,24 @@ watch(focused, (isFocused) => {
     <div class="rel right:50px flex">
       <IconBrandDiscord :size="40"/><h1>我的Discord</h1>
     </div>
-    <iframe class="h:200px w:400px r:10px" src="https://lanyard.cnrad.dev/api/503043347246743567?idleMessage=I%20hate%20my%20life%20"></iframe>
+    <iframe class="h:200px w:400px r:10px shadow:6|4|crimson-58:hover ~300ms|ease-in" src="https://lanyard.cnrad.dev/api/503043347246743567?idleMessage=I%20hate%20my%20life%20"></iframe>
   </div>
   <div class="rel flex align-items:center justify-content:center w:100% p:20">
     <IconBrandX :size="40"/><h1>我的 X (twitter)</h1> <a class="rel color:white left:10px" href="https://twitter.com/TommcyOWO">@TommcyOWO</a>
   </div>
   <div class="rel flex align-items:center justify-content:center w:100% p:20">
-    <IconBrandGithub :size="40"/><h1>我的github</h1> <a class="rel color:white left:10px" href="https://github.com/TommcyOWO/TommcyOWO">@TommcyOWO</a>
+    <IconBrandGithub :size="40"/><h1>我的github</h1> <a class="rel color:white left:10px" href="https://github.com/TommcyOWO">@TommcyOWO</a>
   </div>
   </section>
   <section class="rel w:100% top:100 color:white">
     <div class="rel block text:center">
       <h2>關於Tomycat的介紹：</h2>
-      <p>一位剛15歲國中準考生，正準備面對那<del>可悲</del>國民會考。你對我有興趣?好奇怪的人。</p>
+      <p>一位剛15歲國中準考生，正準備面對那<del class="text-decoration:line-through">可悲</del>國民會考。你對我有興趣?好奇怪的人。</p>
       <p>喜歡研究人類，探討人性，被所有人討厭的超級怪胎 ಥ⌣ಥ</p>
-      <p>參加過110年度貓咪杯，從那時真正開始我的Coding人生到現在!!!</p>
+      <p>參加過110年度貓咪盃，從那時真正開始我的Coding人生到現在!!!</p>
       <p>很有趣對吧?</p>
-      <p>支持居住正義；支持司法改革；支持醫用大麻合法化</p>
+      <p>支持居住正義；支持司法改革；支持醫用大麻合法化；支持多元性別；支持多元性取向</p>
     </div>
   </section>
   <div class="abs top:110vh h:10px w:100%"></div>
 </template>
-<style scoped>
-del{
-  text-decoration: line-through;
-}
-</style>

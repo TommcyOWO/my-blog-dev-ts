a0cd6e27533f72f033e983e51b1d2a43369b1bd4
TommcyOWO <tangminghong.tom@gmail.com>
2023-10-17 22:43:12 +0800

    update

---
+++ new/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex jc:space-between font:medium bd:blur(5px)">
+      <ul class="list-style:none flex f:25px m:10px">
+        <li class="m:10px">test</li>
+        <li class="m:10px">test2</li>
+        <li class="m:10px">test3</li>
+        <li class="m:10px">test4</li>
+    </ul>
+    <div class="flex ai:center m:10px rel right:30px">
+      <IconBlur color="white" class="cursor:pointer"/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import '@master/css';
+import { IconBlur} from '@tabler/icons-vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ new/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <main>
+    <img class="inline-block h:1000px w:100% obj:cover" src="@/assets/bg.jpg">
+  </main>
+
+</template>
+
+<script setup lang="ts">
+import '@master/css';
+</script>
+
+<style scoped>
+
+</style>

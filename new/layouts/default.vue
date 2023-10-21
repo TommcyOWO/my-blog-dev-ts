76d94fe41bc61052702a86471a7770669a1571f9
TommcyOWO <tangminghong.tom@gmail.com>
2023-10-21 19:57:30 +0800

    update

--- new/layouts/default.vue
+++ new/layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex fixed w:100% jc:space-between font:medium bd:blur(5px) z-index:5">
       <div>
-        <IconMenu2 color="white" class="hidden@>md m:20px cursor:pointer"/>
-        <ul class="list-style:none flex@>md f:20px color:gray-80">
+        <IconMenu2 @click="menu_toggle()" color="white" class="hidden@>md m:20px cursor:pointer"/>
+        <ul :class="{'hidden@<md':m_value}" class="list-style:none flex@>md f:20px color:gray-80">
           <li @click="moveTo('index')" :class="{ 'color:crimson-54': $route.path === '/' }" class="m:20px ~300ms|ease-in color:crimson-54:hover cursor:pointer">{{ $t('index') }}</li>
           <li v-for="route in routes" @click="moveTo(route)" :class="{'color:crimson-54': $route.path === '/'+route}" class="m:20px ~300ms|ease-in color:crimson-54:hover cursor:pointer">{{ $t(route) }}</li>
         </ul>
       </div>
-    <div class="flex ai:center@>md m:10px top:10px@<md rel@<md">
+    <div class="flex ai:center@>md m:10px {py:10px;rel}@<md text:center">
       <a href="https://github.com/TommcyOWO#-hi-im-tomycat-一個平平無奇數學爆幹爛的國3生" target="_blank"><IconBrandGithub color="white" class="mx:10px cursor:pointer"/></a>
       <select v-model="locale" class="mx:10px h:25px b:2|solid|crimson-54 bg:white outline:none r:10px">
           <option value="en">English</option>
@@ -26,6 +26,7 @@ const routes = ref(['about','overview_development','works'])
 const { locale } = useI18n();
 const router = useRouter();
 const moveTo = (path:string) => router.push({ name: path });
+const [ m_value,menu_toggle ] = useToggle(true);
 
 </script>
 
--- new/pages/works.vue
+++ new/pages/works.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="bg:gray-16">
-
+  <div class="text:center w:100% abs top:20vh color:white">
+    <section class="p:10vh">
+      <h2>目前空空如也...再等等吧!</h2>
+    </section>
+    <section class="p:10vh">
+      <h2>想看到更多嗎?<br>去我的Github 看看吧!</h2>
+    </section>
   </div>
 </template>
 
